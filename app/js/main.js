d1efbf78291985438a3dcba64658e3e21358c9dd
TheGast <wotthegast@yandex.ru>
2020-09-10 21:39:31 +0300

    added slider-product

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,13 @@
-$(function(){
+$(function() {
 
+    $(".content__stats-rat").rateYo({
+        rating: 3.6,
+        starWidth: '15px',
+    });
 
-
+    $(".slider-product__slider-items").slick({
+        prevArrow: '<button type="button" class="slick-prev slick-arrow"><img src="images/icons/slick-prev.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next slick-arrow"><img src="images/icons/slick-next.svg"></button>'
+    });
 
 });
