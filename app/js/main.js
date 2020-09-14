c10f03bb17481bd899b7d5eb0e1c13fb125e5b1d
TheGast <wotthegast@yandex.ru>
2020-09-14 16:40:34 +0300

    fix slider

--- app/js/main.js
+++ app/js/main.js
@@ -15,7 +15,7 @@ $(function() {
         nextArrow: '<button type="button" class="slick-next slick-arrow"><img src="images/icons/slick-next.svg"></button>'
     });
 
-    $(".slider-followers__slider-items").slick({
+    $(".followers__slider-items").slick({
         prevArrow: '<button type="button" class="slick-prev slick-arrow"><img src="images/icons/slick-prev.svg"></button>',
         nextArrow: '<button type="button" class="slick-next slick-arrow"><img src="images/icons/slick-next.svg"></button>',
         slidesToShow: 3,
